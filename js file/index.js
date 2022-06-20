f6394159744a95cdeb2372a96689c74d67c084ff
EyasuTeshome <57931408+EyasuTeshome@users.noreply.github.com>
2022-06-21 01:57:52 +0800

    delete a directory with a wrong file name format

--- js file/index.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.nav-pic');
-const mobMenu = document.getElementById('menu');
-
-menu.addEventListener('click', () => {
-  mobMenu.classList.toggle('active');
-});
